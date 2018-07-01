9fd78806d36aed0dd5012a52f31b5c41fc02bba0
Samuel Ruiz <samuelruiz30@gmail.com>
2018-06-30 21:38:35 -0700

    Java Game. This is a starter template for a multiplater game

---
+++ JavaGame/src/javagame/JavaGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagame;
+
+/**
+ *
+ * @author zamy0
+ */
+public class JavaGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+}
